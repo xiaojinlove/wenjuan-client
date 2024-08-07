0d68e0afebfc4e12aae1af9828e6d4d33d7cdb5c
gaoyan <magaoyanxj@gmail.com>
2024-08-07 14:20:35 +0800

    chore: 创建form表单-使用隐藏域存储questionId

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,6 +1,7 @@
 import QuestionInput from "@/components/QuestionComponents/QuestionInput"
 import QuestionRadio from "@/components/QuestionComponents/QuestionRadio"
 import Head from "next/head"
+import styles from '../../styles/Question.module.scss'
 
 type PropsType = {
   id: string
@@ -15,20 +16,26 @@ export default function Question(props: PropsType) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Question page</h1>
-        <p>{props.id}</p>
-
         <form>
-          <QuestionInput fe_id="c1" props={{ title: '你的姓名', placeholder: '请输入姓名' }}/>
-          <QuestionRadio fe_id="c2" props={{ 
-            title: '你的性别',
-            options: [
-              { value: 'male', text: '男' },
-              { value: 'female', text: '女' }
-            ],
-            value: 'male',
-            isVertical: false
-          }} />
+          <input type="hidden" name="questionId" defaultValue={props.id} />
+          <div className={styles.componentWrapper}>
+            <QuestionInput fe_id="c1" props={{ title: '你的姓名', placeholder: '请输入姓名' }}/>
+          </div>
+          <div className={styles.componentWrapper}>
+            <QuestionRadio fe_id="c2" props={{ 
+              title: '你的性别',
+              options: [
+                { value: 'male', text: '男' },
+                { value: 'female', text: '女' }
+              ],
+              value: 'male',
+              isVertical: false
+            }} />
+          </div>
+          <div className={styles.submitBtnContainer}>
+            {/* <input type="submit" value="提交" /> */}
+            <button type="submit">提交</button>
+          </div>
         </form>
       </main>
   </>
