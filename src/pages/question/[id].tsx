7dcf57f12dc611b66a3efca2d95d19318c844053
gaoyan <magaoyanxj@gmail.com>
2024-08-07 17:46:54 +0800

    chore: 获取问卷数据，并校验是否正常

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -2,16 +2,51 @@ import QuestionInput from "@/components/QuestionComponents/QuestionInput"
 import QuestionRadio from "@/components/QuestionComponents/QuestionRadio"
 import styles from '../../styles/Question.module.scss'
 import PageWrapper from "@/components/PageWrapper"
+import { getQuestionById } from "@/services/question"
 
 type PropsType = {
-  id: string
+  errno: number
+  data?: {
+    id: string
+    title: string
+    desc?: string
+    js?: string
+    css?: string
+    isPublished: boolean
+    isDeleted: boolean
+    componentList: Array<any>
+  }
+  msg?: string
 }
 
 export default function Question(props: PropsType) {
+  const { errno, data, msg = '' } = props
+
+  if (errno !== 0) {
+    return <PageWrapper title="错误">
+      <h1>错误</h1>
+      <p>{msg}</p>
+    </PageWrapper>
+  }
+
+  const { id, title = '', isDeleted, desc = '', isPublished } = data || {}
+  if (isDeleted) {
+    return <PageWrapper title={title} desc={desc}>
+      <h1>{title}</h1>
+      <p>该问卷已经被删除</p>
+    </PageWrapper>
+  }
+
+  if (!isPublished) {
+    return <PageWrapper title={title} desc={desc}>
+      <h1>{title}</h1>
+      <p>该问卷尚未发布</p>
+    </PageWrapper>
+  }
   return <>
-      <PageWrapper title="question">
+      <PageWrapper title={title}  desc={desc}>
         <form method="post" action="/api/answer">
-          <input type="hidden" name="questionId" defaultValue={props.id} />
+          <input type="hidden" name="questionId" defaultValue={id} />
           <div className={styles.componentWrapper}>
             <QuestionInput fe_id="c1" props={{ title: '你的姓名', placeholder: '请输入姓名' }}/>
           </div>
@@ -38,9 +73,9 @@ export default function Question(props: PropsType) {
 export async function getServerSideProps(context: any) {
   const { id = '' } = context.params
 
+  const data = await getQuestionById(id)
+
   return {
-    props: {
-      id,
-    }
+    props: data
   }
 }
